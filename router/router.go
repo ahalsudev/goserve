2c7e4933d1faced12232fa4a591ffba00ef1c5b8
Ahmad <ahalsu67@gmail.com>
2022-11-20 10:53:07 +0300

    Add isPrime

--- router/router.go
+++ router/router.go
@@ -1,6 +1,10 @@
 package router
 
-import "net/http"
+import (
+	"math"
+	"net/http"
+	"strconv"
+)
 
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
@@ -8,5 +12,33 @@ func NewRouter() http.Handler {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Ok"))
 	})
+
+	mux.HandleFunc("/calcPrimes/", func(w http.ResponseWriter, r *http.Request) {
+		primes := calcPrimes(10, 10000)
+		w.WriteHeader(http.StatusOK)
+		bs := []byte(strconv.Itoa(primes))
+		w.Write(bs)
+	})
+
 	return mux
+}
+
+func calcPrimes(s int, e int) int {
+	sum := 0
+	for i := s; i <= e; i++ {
+		prime := isPrime(i)
+		if prime {
+			sum++
+		}
+	}
+	return sum
+}
+
+func isPrime(n int) bool {
+	for i := 2; i <= int(math.Sqrt(float64(n))); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
